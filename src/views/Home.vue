0f8448a398535b3fb43ae268c49eae5a1f0f766b
gdlyeabkov <gdlyeabkov@gmail.com>
2021-11-09 09:26:42 +0300

    addshortcuts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,108 +4,9 @@
     <audio ref="bus" src="../../sounds/bus.mp3" class="bus" />
     <audio id="upperBus" ref="upperBus" src="../../sounds/upperBus.mp3" class="upperBus" />
     <div class="piano">
-      <div @click="playSound($event)" id="pianoKey" class="pianoKey">
-        <div @click="playUpperSound()" class="pianoUpperKey">
+      <div v-for="pianoKey in pianoKeys" :id="`pianoKeyId_${busHotkeys[pianoKey]}`" :key="pianoKey" @click="playSound(2 / pianoKeys * pianoKey)" class="pianoKey" :data-bus-hot-keys-index="pianoKey" data-bus-is-minor="true">
+        <div v-if="pianoKey < pianoKeys" @click="playUpperSound(2 / pianoKeys * pianoKey)" class="pianoUpperKey"  :id="`pianoKeyId_${upperBusHotkeys[pianoKey]}`" :data-bus-hot-keys-index="pianoKey" data-bus-is-major="true">
         </div>
-      </div>
-      <div @click="playSound($event)" id="pianoKey" class="pianoKey">
-        <div @click="playUpperSound()" class="pianoUpperKey">
-        </div>
-      </div>
-      <div @click="playSound($event)" id="pianoKey" class="pianoKey">
-        <div @click="playUpperSound()" class="pianoUpperKey">
-        </div>
-      </div>
-      <div @click="playSound($event)" id="pianoKey" class="pianoKey">
-        <div @click="playUpperSound()" class="pianoUpperKey">
-        </div>
-      </div>
-      <div @click="playSound($event)" id="pianoKey" class="pianoKey">
-        <div @click="playUpperSound()" class="pianoUpperKey">
-        </div>
-      </div>
-      <div @click="playSound($event)" id="pianoKey" class="pianoKey">
-        <div @click="playUpperSound()" class="pianoUpperKey">
-        </div>
-      </div>
-      <div @click="playSound($event)" id="pianoKey" class="pianoKey">
-        <div @click="playUpperSound()" class="pianoUpperKey">
-        </div>
-      </div>
-      <div @click="playSound($event)" id="pianoKey" class="pianoKey">
-        <div @click="playUpperSound()" class="pianoUpperKey">
-        </div>
-      </div>
-      <div @click="playSound($event)" id="pianoKey" class="pianoKey">
-        <div @click="playUpperSound()" class="pianoUpperKey">
-        </div>
-      </div>
-      <div @click="playSound($event)" id="pianoKey" class="pianoKey">
-        <div @click="playUpperSound()" class="pianoUpperKey">
-        </div>
-      </div>
-      <div @click="playSound($event)" id="pianoKey" class="pianoKey">
-        <div @click="playUpperSound()" class="pianoUpperKey">
-        </div>
-      </div>
-      <div @click="playSound($event)" id="pianoKey" class="pianoKey">
-        <div @click="playUpperSound()" class="pianoUpperKey">
-        </div>
-      </div>
-      <div @click="playSound(event)" class="pianoKey">
-        <div @click="playUpperSound()" class="pianoUpperKey">
-        </div>
-      </div>
-      <div @click="playSound()" class="pianoKey">
-        <div @click="playUpperSound()" class="pianoUpperKey">
-        </div>
-      </div>
-      <div @click="playSound()" class="pianoKey">
-        <div @click="playUpperSound()" class="pianoUpperKey">
-        </div>
-      </div>
-      <div @click="playSound()" class="pianoKey">
-        <div @click="playUpperSound()" class="pianoUpperKey">
-        </div>
-      </div>
-      <div @click="playSound()" class="pianoKey">
-        <div @click="playUpperSound()" class="pianoUpperKey">
-        </div>
-      </div>
-      <div @click="playSound()" class="pianoKey">
-        <div @click="playUpperSound()" class="pianoUpperKey">
-        </div>
-      </div>
-      <div @click="playSound()" class="pianoKey">
-        <div @click="playUpperSound()" class="pianoUpperKey">
-        </div>
-      </div>
-      <div @click="playSound()" class="pianoKey">
-        <div @click="playUpperSound()" class="pianoUpperKey">
-        </div>
-      </div>
-      <div @click="playSound()" class="pianoKey">
-        <div @click="playUpperSound()" class="pianoUpperKey">
-        </div>
-      </div>
-      <div @click="playSound()" class="pianoKey">
-        <div @click="playUpperSound()" class="pianoUpperKey">
-        </div>
-      </div>
-      <div @click="playSound()" class="pianoKey">
-        <div @click="playUpperSound()" class="pianoUpperKey">
-        </div>
-      </div>
-      <div @click="playSound()" class="pianoKey">
-        <div @click="playUpperSound()" class="pianoUpperKey">
-        </div>
-      </div>
-      <div @click="playSound()" class="pianoKey">
-        <div @click="playUpperSound()" class="pianoUpperKey">
-        </div>
-      </div>
-      <div @click="playSound()" class="pianoKey">
-        
       </div>
     </div>
     <Footer />
@@ -118,17 +19,96 @@ import Footer from '@/components/Footer.vue'
 
 export default {
   name: 'Home',
+  data(){
+    return {
+      pianoKeys: 26,
+      busHotkeys: {
+        1: 'q',
+        2: 'w',
+        3: 'e',
+        4: 'r',
+        5: 't',
+        6: 'y',
+        7: 'u',
+        8: 'i',
+        9: 'o',
+        10: 'p',
+        11: '[',
+        12: ']',
+        13: 'a',
+        14: 's',
+        15: 'd',
+        16: 'f',
+        17: 'g',
+        18: 'h',
+        19: 'j',
+        20: 'k',
+        21: 'l',
+        22: '\;',
+        23: '\'',
+        24: 'z',
+        25: 'x',
+        26: 'c'
+      },
+      upperBusHotkeys: {
+        1: '\$',
+        2: 'v',
+        3: 'b',
+        4: 'n',
+        5: 'm',
+        6: '\,',
+        7: '\.',
+        8: '\/',
+        9: '1',
+        10: '2',
+        11: '3',
+        12: '4',
+        13: '5',
+        14: '6',
+        15: '7',
+        16: '8',
+        17: '9',
+        18: '\/',
+        19: '\*',
+        20: '\-',
+        21: '\+',
+        22: '\`',
+        23: 'Escape',
+        24: 'Enter',
+        25: '\%'
+      }
+    }
+  },
   mounted(){
-
+    document.body.addEventListener('keyup', event => {
+      let activePianoKey = document.getElementById(`pianoKeyId_${event.key}`)
+      if(activePianoKey.hasAttribute('data-bus-is-minor')) {
+        activePianoKey.style.backgroundColor = 'rgb(225, 225, 225)'
+      } else if(activePianoKey.hasAttribute('data-bus-is-major')) {
+        activePianoKey.style.backgroundColor = 'rgb(125, 125, 125)'
+      }
+      this.playSound(2 / this.pianoKeys * activePianoKey.getAttribute('data-bus-hot-keys-index'))
+      if(activePianoKey.hasAttribute('data-bus-is-minor')) {
+        setTimeout(() => activePianoKey.style.backgroundColor = 'rgb(255, 255, 255)', 300)
+      } else if(activePianoKey.hasAttribute('data-bus-is-major')) {
+        setTimeout(() => activePianoKey.style.backgroundColor = 'rgb(0, 0, 0)', 300)
+      }
+    })
   },
   methods: {
-    playSound(event){
-      this.$refs.bus.play()
+    playSound(rate){
+      // this.$refs.bus.play()
+      let minor = new Audio(this.$refs.bus.src)
+      minor.playbackRate = rate
+      minor.play()
     },
-    playUpperSound() {
+    playUpperSound(rate) {
       // console.log(`this.$refs.upperBus: ${Object.values(this.$refs.upperBus)}`)
       console.log(`this.$refs.upperBus: ${document.getElementById("upperBus").volume}`)
-      this.$refs.upperBus.play()
+      // this.$refs.upperBus.play()
+      let major = new Audio(this.$refs.upperBus.src)
+      major.playbackRate = rate
+      major.play()
     }
   },
   components: {
@@ -151,6 +131,15 @@ export default {
     border: 1px solid rgb(215, 215, 215);
     width: 25px;
     cursor: pointer;
+    background-color: rgb(255, 255, 255);
+  }
+
+  .pianoKey:hover {
+    background-color: rgb(240, 240, 240);
+  }
+
+  .pianoKey:active {
+    background-color: rgb(225, 225, 225);
   }
 
   .pianoUpperKey {
@@ -161,6 +150,14 @@ export default {
     position: relative;
     left: 5px;
     border-radius: 0px 0px 5px 5px;
+  }
+
+  .pianoUpperKey:hover {
+    background-color: rgb(75, 75, 75);
+  }
+
+  .pianoUpperKey:active {
+    background-color: rgb(125, 125, 125);
   }
 
   .bus, .upperBus {
